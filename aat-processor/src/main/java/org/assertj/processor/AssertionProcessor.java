c6c9631ae3b4664b6e15610530da78f232b0c4e9
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-09-14 13:37:18 +0200

    processor: Add basic AssertionProcessor class
    just prints "Hello World!" for each processing round for now

---
+++ aat-processor/src/main/java/org/assertj/processor/AssertionProcessor.java
@@ -0,0 +1,44 @@
+package org.assertj.processor;
+
+import com.google.auto.service.AutoService;
+import org.assertj.annotation.Assertion;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public class AssertionProcessor extends AbstractProcessor {
+
+    public AssertionProcessor() {
+        super();
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("Hello World!");
+        return true;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotations = new LinkedHashSet<String>();
+        annotations.add(Assertion.class.getCanonicalName());
+        return annotations;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+}
